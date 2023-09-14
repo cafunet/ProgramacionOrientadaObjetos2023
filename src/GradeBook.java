51d694d38df4445bbb71f31498eea2de6c877fb7
cafunet <juanpaco30@hotmail.com>
2023-09-14 14:09:12 +0200

    Actualizacion pimer commit

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,4 +1,31 @@
 
 public class GradeBook {
+	
+	private String courseName;
+	
+	//zona de constructores
+	public GradeBook()
+	{
+	}
+	//param courseName
+	 public GradeBook(String courseName) {
+		//super no tiene relevancia ya que se toma como metodo de herencia y son por default al generarse autom.
+		 super();
+		this.courseName = courseName;
+	}
+	 
+	//zona getters y setters  
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public void displayMessage(String course)
+	 {
+		System.out.print("hello from GreenBook class " + getCourseName());
+	 }
 
 }
--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class GradeBookTest {
+	public GradeBookTest() {
+		
+	}
+	public static void main (String[] args )
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		GradeBook gradebook = new GradeBook();
+		
+		System.out.print("give a course name: ");
+		
+		String courseName = sc.nextLine();
+		gradebook.setCourseName(courseName);
+		System.out.println("nombre curso desde el main()" + gradebook.getCourseName());
+		System.out.println("nombre curso con constructor"+ gradebook);
+		gradebook.displayMessage(courseName);
+
+	}
 
 }
