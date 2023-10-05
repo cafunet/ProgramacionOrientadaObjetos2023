994dd1a46261d3e17b4a8707ecb3cff346a75b49
cafunet <juanpaco30@hotmail.com>
2023-10-04 19:08:12 -0500

    actualizacion 041023

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,31 +1,52 @@
+import java.util.Scanner;
 
 public class GradeBook {
 	
 	private String courseName;
 	
-	//zona de constructores
+	//zona de constructores 
 	public GradeBook()
 	{
 	}
-	//param courseName
-	 public GradeBook(String courseName) {
+	//param courseName sirve para crear el atributo y asignarle el valor de tipo de dato
+	public GradeBook(String courseName) {
 		//super no tiene relevancia ya que se toma como metodo de herencia y son por default al generarse autom.
-		 super();
+		super();
 		this.courseName = courseName;
 	}
-	 
 	//zona getters y setters  
 	public String getCourseName() {
 		return courseName;
 	}
-
+//el void es vacio que no devuelve nada y esto permite modificar el atributo creado
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
-
-	public void displayMessage(String course)
-	 {
+// zona logica de negocio
+	public void displayMessage(String course){
 		System.out.print("hello from GreenBook class " + getCourseName());
 	 }
+	
+	// metodo para promedio de calificacines
+	public void determineClassAverage() {
+
+	Scanner sc = new Scanner(System.in);
+	
+	//declarar atributos a utilizar e inicializarlos
+	int total= 0, grade, gradeCounter=1, average;
+	
+	while (gradeCounter <= 5 ) {
+		System.out.println("por favor ingrese notas: "); // pedir info al usuario
+		grade = sc.nextInt(); //permite que el usuario ingrese la info
+		total = total + grade;
+		gradeCounter = gradeCounter + 1;
+		
+	}
+	//promedio = sumatorio de muestras 
+	average = total/5;
+	
+	System.out.println("el promedio de las notas es: " + average);
+	
+	}
 
 }
--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -9,15 +9,19 @@ public class GradeBookTest {
 		Scanner sc = new Scanner(System.in);
 		
 		GradeBook gradebook = new GradeBook();
+		GradeBook gradebookProgramacion00 = new GradeBook ("programacion orientada a objetos");
 		
-		System.out.print("give a course name: ");
+		//gradebook.determineClassAverage();
+		gradebookProgramacion00.determineClassAverage();
 		
+		/*
+		System.out.print("ingrese nombre del curso: ");
 		String courseName = sc.nextLine();
 		gradebook.setCourseName(courseName);
-		System.out.println("nombre curso desde el main()" + gradebook.getCourseName());
-		System.out.println("nombre curso con constructor"+ gradebook);
+		System.out.println("nombre curso desde el main(): " + gradebook.getCourseName());
+		System.out.println("nombre curso con constructor: " + gradebookProgramacion00.getCourseName());
 		gradebook.displayMessage(courseName);
-
+		*/
 	}
 
 }
